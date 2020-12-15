194f59432e4ed7ac22ce2fee61205036bbe1f89a
anettebautista <anette.bautista@scholar.smashprogram.org>
2020-12-15 11:51:44 -0800

    Created circle

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,24 @@
 MuseumHeist = {
+  
+  container: document.getElementById("circle_container"),
 
     init: function() {
 
+      this.createCircle();
+      this.render();
     },
 
     createCircle: function() {
       let circlediv = document.createElement("div");
       circlediv.className = "circle";
+      this.container.append(circlediv);
       let circle = {
-        x_pos: undefined,
-        y_pos: undefined,
+        x_pos: Math.random()*200,
+        y_pos: Math.random()*200,
         x_velocity: 1,
         y_velocity: 1, 
         radius: 10,
-        color: "green",
+        color: "black",
         element: circlediv,
       }
       return circle;
@@ -28,11 +33,12 @@ MuseumHeist = {
     },
 
     render: function() {
-      
+      this.circle.element.style.top= this.circle.y_pos + "px";
+      this.circle.element.style.top = this.circle.x_pos + "px";
     },
 
     changeColor: function() {
-      
+
     },
     
   

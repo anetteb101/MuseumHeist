0ec26e2097ada41fef3f263bd520063e16929b2b
anettebautista <anette.bautista@scholar.smashprogram.org>
2020-12-15 09:58:49 -0800

    Created circle and edited html and css

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,22 @@
 MuseumHeist = {
 
     init: function() {
+      
     },
 
     createCircle: function() {
-
+      let circlediv = document.createElement("div");
+      circlediv.className = "circle";
+      let circle = {
+        x_pos: undefined,
+        y_pos: undefined,
+        x_velocity: 1,
+        y_velocity: 1, 
+        radius: 10,
+        color: "green",
+        element: circlediv,
+      }
+      return circle;
     },
 
     createWalls: function() {
